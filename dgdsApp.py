31312f1bc5359309766369c39d6610a4739ef302
jsalcal <jsalcal@gmail.com>
2019-04-01 16:43:06 +0200

    added config.ini

--- dgdsApp.py
+++ dgdsApp.py
@@ -1,7 +1,7 @@
 # LIBS
 import json
 import os
-import socket
+import configparser
 
 # FLASK
 from flask import Flask
@@ -15,14 +15,17 @@ from dgdsPiServiceDDL import *
 application = Flask(__name__)
 CORS(application)
 
-# Current working directory
+# FLASK app settings
 APP_DIR = os.path.dirname(os.path.realpath(__file__))
+CONFIG = configparser.ConfigParser()
+CONFIG.read(os.path.join(APP_DIR, 'config.ini'))
+HOSTNAME_URL = 'http://{host}:{port}'.format(
+	prot=CONFIG['flask']['protocol'],
+	host=CONFIG['flask']['hostname'],
+	port=CONFIG['flask']['port'])
 
-# PiSerivceUrl-DDL: for now hardcoded. Config file? Services?
+# PISERVICE-DDL: for now hardcoded. Config file? Services?
 PISERVICE_URL = 'http://pl-tc012.xtr.deltares.nl:8080/FewsWebServices/rest/digitaledelta/2.0'
-
-# Hostname URL: Also hard-coded for now, sorry
-HOSTNAME_URL = 'http://{host}:5000'.format(host=socket.gethostname())
 
 # JSON input to dict
 def readInputJSON():
@@ -85,6 +88,14 @@ def timeseries():
 def dummyTimeseries():
 	# Return dummy file contents
 	with open(os.path.join(APP_DIR, './dummyData/dummyTseries.json')) as f:
+		content = json.load(f)
+	return prepareOutputJSON(content, 200)
+
+# Datasets query / all
+@application.route('/datasets', methods=['GET'])
+def datasets():
+	# Return dummy file contents
+	with open(os.path.join(APP_DIR, './configData/datasets.json')) as f:
 		content = json.load(f)
 	return prepareOutputJSON(content, 200)
 
