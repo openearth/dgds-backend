d4762ac6c44b35b852931d8e129f8558c86ec8a1
ckrogers <rogersckw@gmail.com>
2019-08-08 13:57:35 +0200

    Add currents processing to workflow, delete files already uploaded to gee in gee folder

--- datasets/glossis/glossis.py
+++ datasets/glossis/glossis.py
@@ -10,6 +10,7 @@ from google.cloud import storage
 import rasterio
 
 from wind import glossis_wind_to_tiff
+from currents import glossis_currents_to_tiff
 from waterlevel import glossis_waterlevel_to_tiff
 
 
@@ -20,6 +21,24 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     blob = bucket.blob(destination_blob_name)
     print("Uploading from {} to {}/{}".format(source_file_name, bucket_name, destination_blob_name))
     blob.upload_from_filename(source_file_name)
+
+def list_blobs(bucket_name, folder_name):
+    """Lists all the blobs in the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    # Note: Client.list_blobs requires at least package version 1.17.0.
+    blobs = storage_client.list_blobs(bucket, prefix=folder_name)
+
+    # for blob in blobs:
+    #     print(blob.name)
+    return blobs
+
+# def delete_blob(bucket_name, blob):
+#     """Deletes a blob from the bucket."""
+    # storage_client = storage.Client()
+    # bucket = storage_client.get_bucket(bucket_name)
+    # blob = bucket.blob(str(blob_name))
+
 
 
 def upload_to_gee(filename, bucket, asset):
@@ -50,14 +69,16 @@ def upload_to_gee(filename, bucket, asset):
                r"-p fews_implementation_version={} " \
                r"-p fews_patch_number={} " \
                r"-p institution={} " \
+               r"-p analysis_time={} " \
+               r"--time_start {} " \
                r"{}".format(
                    metadata['date_created'],
                    metadata['fews_build_number'],
                    metadata['fews_implementation_version'],
                    metadata['fews_patch_number'],
                    metadata['institution'],
-                   # metadata['system_time_start'],
-                   # metadata['analysis_time'],
+                   metadata['analysis_time'],
+                   metadata['system_time_start'],
                    asset)
 
     print(gee_meta)
@@ -81,8 +102,17 @@ if __name__ == '__main__':
         rmtree(tmpdir)  # could remain from previous triggers
     makedirs(tmpdir)
 
+    # clear items in gee folder in bucket
+    old_blobs = list_blobs(args.bucket[0], "gee")
+    for blob in old_blobs:
+        blob.delete()
+        print('Blob {} deleted.'.format(blob))
+
     waterlevel_tiff_fn = glossis_waterlevel_to_tiff(args.bucket[0], args.prefix[0], tmpdir)
-    upload_to_gee(waterlevel_tiff_fn, args.bucket[0], args.assetfolder[0]+"/waterlevel/waterlevel")
+    upload_to_gee(waterlevel_tiff_fn, args.bucket[0], args.assetfolder[0]+"/waterlevel/"+waterlevel_tiff_fn.replace(".tif", ""))
+
+    current_tiff_fn = glossis_currents_to_tiff(args.bucket[0], args.prefix[0], tmpdir)
+    upload_to_gee(current_tiff_fn, args.bucket[0], args.assetfolder[0] + "/currents/"+current_tiff_fn.replace(".tif", ""))
 
     wind_tiff_fn = glossis_wind_to_tiff(args.bucket[0], args.prefix[0], tmpdir)
-    upload_to_gee(wind_tiff_fn, args.bucket[0], args.assetfolder[0]+"/wind/wind")
+    upload_to_gee(wind_tiff_fn, args.bucket[0], args.assetfolder[0]+"/wind/wind"+wind_tiff_fn.replace(".tif", ""))
