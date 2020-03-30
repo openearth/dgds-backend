f7bd1af60b66fc0d891d95e218c04fb6db6758db
Fedor Baart <fedor.baart@deltares.nl>
2020-03-30 15:50:52 +0200

    update script

--- datasets/glossis/glossis2flowmap.py
+++ datasets/glossis/glossis2flowmap.py
@@ -1,26 +1,147 @@
 #!/usr/bin/env python
 # coding: utf-8
+import logging
 import pathlib
+import time
 
 import click
 import rasterio
 import numpy as np
 import matplotlib.pyplot as plt
+import ee
+import google.cloud.storage
 
 from PIL import Image
 
-height = 180
-width = 360
+logger = logging.getLogger(__name__)
+
+def getWGS84Geometry():
+    geometry = ee.Geometry.Polygon([
+            [180,90],
+            [0,90],
+            [-180,90],
+            [-180,-90],
+            [0,-90],
+            [180,-90],
+            [180,90]
+        ],
+        'EPSG:4326',
+        False
+    )
+    return geometry
+
+def last(images):
+    sorted = images.sort('system:time_start', False)
+    last = sorted.first()
+    return last
+
+def tail(images, n):
+    sorted = images.limit(n, 'system:time_start', False)
+    return sorted
 
 
-@click.command()
-@click.argument('input', type=click.Path(exists=True))
-@click.option('--max-zoom', type=int, default=4)
-def cli(input, max_zoom):
+def computeFlowmap(currents):
+    """Extract the timestamp for unique variable names  (assumes new instances are overwritten)"""
+    timeStamp = currents.date().format("yyyyMMddHHmm")
+    currents = currents.set('time_stamp', timeStamp)
+    flowmap = currents.unitScale(-0.5, 0.5)
+
+    # First deal with the mask
+    # Create unmask and replace  mask by missing value
+    flowmap = flowmap.unmask(-9999)
+
+    # take  the mask
+    mask = flowmap.eq(-9999).select('b1')
+    # Create a smooth mask
+    mask = mask.float()
+
+    land = ee.Image("users/gena/land_polygons_image")
+
+    mask = (
+      mask
+        .resample('bilinear')
+        .where(land.mask(), 1)
+        .unmask()
+    )
+
+    # convert to 0,1
+    flowmap = flowmap.clamp(0, 1)
+    flowmap = flowmap.resample('bilinear')
+
+    flowmap = (
+      flowmap
+        .convolve(ee.Kernel.gaussian(30000, 20000, 'meters'))
+    )
+
+    flowmap = flowmap.addBands(mask)
+    flowmap = flowmap.rename(['u', 'v', 'mask'])
+    # Convert three  channels to rgb
+    flowmapRgb = flowmap.visualize()
+    flowmapRgb = ee.Image(
+        flowmapRgb
+          .copyProperties(currents)
+    )
+    return flowmapRgb
+
+
+
+def exportFlowmap():
+    """export the last flowmap"""
+    ee.Initialize()
+    glossis = ee.ImageCollection("projects/dgds-gee/glossis/currents")
+
+    region = getWGS84Geometry()
+    currentImage = last(glossis)
+
+    flowmapRgb = computeFlowmap(currentImage)
+
+    # Extract all time_stamps
+    timeStamp = flowmapRgb.get('time_stamp').getInfo()
+
+    exportFilename ='glossis-current' + '-' + timeStamp
+    kwargs = {
+        "image": flowmapRgb,
+        "description": exportFilename + '-flowmap',
+        "bucket": 'deltares-video-map',
+        "fileNamePrefix": exportFilename,
+        "dimensions": '8192x5760',
+        "region": region,
+        "crs": 'EPSG:4326'
+    }
+    logger.debug('exporting task {}'.format(kwargs))
+    task = ee.batch.Export.image.toCloudStorage(**kwargs)
+
+    task.start()
+
+    return task
+
+def downloadBlob(filename):
+    bucket_name = 'deltares-video-map'
+
+    client = google.cloud.storage.Client()
+    bucket = client.get_bucket(bucket_name)
+    blob = bucket.blob(filename)
+    blob.download_to_filename(filename)
+
+def uploadDirectory(dir_name):
+    bucket_name = 'deltares-video-map'
+    cmd = 'gsutil -m cp -r {dir_name} gs://deltares-video-map/glossis/{dir_name}'.format(dir_name=dir_name)
+    process = subprocess.Popen(cmd)
+
+
+
+def tiff2tiles(input, max_zoom=6):
+    """convert a tiff file to a set of flowmap  tiles according to the windgl format"""
+    # TODO: compute max_zoom
+
+    # fixed settings
+    height = 180
+    width = 360
+
     ds = rasterio.open(input)
 
     input_path = pathlib.Path(input)
-    
+
     currents_r = ds.read(1)
     currents_g = ds.read(2)
     currents_b = ds.read(3)
@@ -48,6 +169,28 @@ def cli(input, max_zoom):
                 img_xyz = img_at_zoom[s]
                 plt.imsave(filename, img_xyz)
 
+
+@click.group()
+def cli():
+    logging.basicConfig(level=logging.DEBUG)
+
+@cli.command()
+@click.argument('filename')
+def tiles(filename):
+    downloadBlob(filename)
+    tiff2tiles(filename)
+    uploadBlob(pathlib.Path(filename.with_suffix('')))
+
+@cli.command()
+def flowmap():
+    task = exportFlowmap()
+    # wait for task to finish
+    logger.info('Submitted  EE Task {}: {}'.format(task.id, task.name))
+    # task submission is not received  (UNSUBMITTED), task is waiting  to run  (READY) or task is done (READY)
+    while task.state in ('READY', 'RUNNING', 'UNSUBMITTED'):
+        time.sleep(5)
+        task.status()
+    logger.info('EE Task {} completed with state {}'.format(task.id, task.state))
 
 if  __name__ == '__main__':
     cli()
