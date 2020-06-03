ea487801e41696a6bc27d148d646a4581ed1d82f
Fedor Baart <fedor.baart@deltares.nl>
2020-06-03 10:16:35 +0200

    conditionaly  add flowmap information

--- dgds_backend/app.py
+++ dgds_backend/app.py
@@ -36,13 +36,15 @@ app.config.update({
 docs = FlaskApiSpec(app)
 
 # Configuration load
-app.register_blueprint(error_handler.error_handler)
 app.config.from_object("dgds_backend.default_settings")
 try:
     app.config.from_envvar("DGDS_BACKEND_SETTINGS")
 except (RuntimeError, FileNotFoundError) as e:
     print("Could not load config from environment variables")  # logging not set yet [could not read config]
 
+# only catch error if we're not in debug mode
+if not app.debug:
+    app.register_blueprint(error_handler.error_handler)
 
 # Logging setup
 if not app.debug:
@@ -107,6 +109,7 @@ def timeseries(**input):
 
 
 @app.route("/datasets", methods=["GET"])
+# cache this request so it returns the same result for 6 hours.
 @cache.cached(timeout=6 * 60 * 60, key_prefix="datasets")
 @marshal_with(DatasetSchema(many=True))
 def datasets():
@@ -114,16 +117,20 @@ def datasets():
     Get datasets, populated with applicable urls for each dataset. Cached on 6hr intervals,
     based on time between new GLOSSIS files created
     """
+    # TODO: move this out of here and into a task (with celery or something)
 
     # Loop over datasets
     for datasetinfo in DATASETS["info"]["datasets"]:
         id = datasetinfo["id"]
-        data = dataset(id, None)
-        datasetinfo.update(data)
-        raster_layer = datasetinfo.get("rasterLayer", {})
-        raster_layer.update(data)
-        datasetinfo['rasterLayer'] = raster_layer
 
+        # we have datasetinfo from the json file
+        # also get more information by calling the /dataset url
+        data = dataset(id, None)
+
+        # update the dataset with relevant info from the Url
+        datasetinfo.update(data)
+
+    #  all of the above is inline, so we can return the original object.
     return jsonify(DATASETS["info"])
 
 
@@ -154,15 +161,19 @@ def dataset(datasetId, imageId):
 
     # Populate flowmapLayer information
     service_url_data = get_service_url(datasetId, "flowmapService")
-    access_url, name, protocol, parameters = service_url_data["url"], service_url_data["name"], \
-                                             service_url_data["protocol"], service_url_data["parameters"]
-    if protocol == "googlestorage":
-        flowmap_data = get_google_storage_url(datasetId, name, access_url, parameters)
-    else:
-        logging.error("{} protocol not recognized for flowmap datasetId {}".format(protocol, datasetId))
-        flowmap_data = {}
+    if service_url_data:
+        access_url = service_url_data["url"]
+        name = service_url_data["name"]
+        protocol = service_url_data["protocol"]
+        parameters = service_url_data["parameters"]
 
-    dataset_dict["flowmapLayer"] = flowmap_data
+        if protocol == "googlestorage":
+            flowmap_data = get_google_storage_url(datasetId, name, access_url, parameters)
+        else:
+            logging.error("{} protocol not recognized for flowmap datasetId {}".format(protocol, datasetId))
+            flowmap_data = {}
+
+        dataset_dict["flowmapLayer"] = flowmap_data
 
     return dataset_dict
 
