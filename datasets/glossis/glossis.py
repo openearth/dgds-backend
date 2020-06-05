9d236de01f3d74ca827979e10e06474479838e6b
Fedor Baart <fedor.baart@deltares.nl>
2020-06-05 17:34:08 +0200

    download tiffs,  before processing

--- datasets/glossis/glossis.py
+++ datasets/glossis/glossis.py
@@ -7,7 +7,7 @@ from os.path import exists
 from pathlib import Path
 from shutil import rmtree
 
-from utils import fm_to_tiff, list_blobs, upload_to_gee, wait_gee_tasks, upload_dir_to_bucket
+from utils import fm_to_tiff, list_blobs, upload_to_gee, wait_gee_tasks, upload_dir_to_bucket, download_blob
 from waterlevel import create_water_level_astronomical_band
 from waveheight import glossis_waveheight_to_tiff
 from wind import glossis_wind_to_tiff
@@ -28,6 +28,10 @@ if __name__ == "__main__":
         "prefix", type=str, nargs=1, help="Input folder/prefix", default="fews_glossis/"
     )
     parser.add_argument("assetfolder", type=str, nargs=1, help="GEE asset")
+    parser.add_argument("--skip-waterlevel", dest='skip_waterlevel', default=False, action='store_true')
+    parser.add_argument("--skip-wind", dest='skip_wind', default=False, action='store_true')
+    parser.add_argument("--skip-currents", dest='skip_currents', default=False, action='store_true')
+    parser.add_argument("--skip-waves", dest='skip_waves', default=False, action='store_true')
 
     args = parser.parse_args()
     logging.info(args.bucket)
@@ -50,104 +54,115 @@ if __name__ == "__main__":
 
     taskids = []
 
-    waterlevel_tiff_filenames = fm_to_tiff(
-        bucket,
-        args.prefix[0],
-        tmpdir,
-        variables=["water_level_surge", "water_level"],
-        filter="waterlevel",
-        output_fn="glossis_waterlevel",
-        nodata=-9999,
-        extra_bands=1,  # for astronomical tide
-    )
+    if not args.skip_waterlevel:
 
-    # Update third band in rasters
-    for waterlevel_tiff_filename in waterlevel_tiff_filenames:
-        create_water_level_astronomical_band(waterlevel_tiff_filename)
-
-    for file in waterlevel_tiff_filenames:
-        taskid = upload_to_gee(
-            file,
+        waterlevel_tiff_filenames = fm_to_tiff(
             bucket,
-            args.assetfolder[0] + "/waterlevel/" + file.replace(".tif", ""),
-            wait=False,
-            force=True,
+            args.prefix[0],
+            tmpdir,
+            variables=["water_level_surge", "water_level"],
+            filter="waterlevel",
+            output_fn="glossis_waterlevel",
+            nodata=-9999,
+            extra_bands=1,  # for astronomical tide
         )
-        logging.info(f"Added task {taskid}")
-        taskids.append(taskid)
 
-    current_tiff_filenames = fm_to_tiff(
-        bucket,
-        args.prefix[0],
-        tmpdir,
-        variables=["currents_u", "currents_v"],
-        filter="currents",
-        output_fn="glossis_currents",
-        nodata=-9999,
-    )
+        # Update third band in rasters
+        for waterlevel_tiff_filename in waterlevel_tiff_filenames:
+            create_water_level_astronomical_band(waterlevel_tiff_filename)
 
-    # these assets are needed by the flowmap
-    current_assets = []
-    for file in current_tiff_filenames:
-        current_asset = args.assetfolder[0] + "/currents/" + file.replace(".tif", "")
-        taskid = upload_to_gee(
-            file,
+        for file in waterlevel_tiff_filenames:
+            taskid = upload_to_gee(
+                file,
+                bucket,
+                args.assetfolder[0] + "/waterlevel/" + file.replace(".tif", ""),
+                wait=False,
+                force=True,
+            )
+            logging.info(f"Added task {taskid}")
+            taskids.append(taskid)
+
+    if not args.skip_currents:
+        current_tiff_filenames = fm_to_tiff(
             bucket,
-            current_asset,
-            wait=False,
-            force=True,
+            args.prefix[0],
+            tmpdir,
+            variables=["currents_u", "currents_v"],
+            filter="currents",
+            output_fn="glossis_currents",
+            nodata=-9999,
         )
-        logging.info(f"Added task {taskid}")
-        current_assets.append(current_asset)
-        taskids.append(taskid)
 
-    wind_tiff_filenames = glossis_wind_to_tiff(bucket, args.prefix[0], tmpdir)
+        # these assets are needed by the flowmap
+        current_assets = []
+        for file in current_tiff_filenames:
+            current_asset = args.assetfolder[0] + "/currents/" + file.replace(".tif", "")
+            taskid = upload_to_gee(
+                file,
+                bucket,
+                current_asset,
+                wait=False,
+                force=True,
+            )
+            logging.info(f"Added task {taskid}")
+            current_assets.append(current_asset)
+            taskids.append(taskid)
 
-    for file in wind_tiff_filenames:
-        taskid = upload_to_gee(
-            file,
-            bucket,
-            args.assetfolder[0] + "/wind/" + file.replace(".tif", ""),
-            wait=False,
-            force=True,
+    if not args.skip_wind:
+        wind_tiff_filenames = glossis_wind_to_tiff(bucket, args.prefix[0], tmpdir)
+
+        for file in wind_tiff_filenames:
+            taskid = upload_to_gee(
+                file,
+                bucket,
+                args.assetfolder[0] + "/wind/" + file.replace(".tif", ""),
+                wait=False,
+                force=True,
+            )
+            logging.info(f"Added task {taskid}")
+            taskids.append(taskid)
+
+    if not args.skip_waves:
+
+        waveheight_tiff_filenames = glossis_waveheight_to_tiff(
+            bucket, args.prefix[0], tmpdir
         )
-        logging.info(f"Added task {taskid}")
-        taskids.append(taskid)
 
-    waveheight_tiff_filenames = glossis_waveheight_to_tiff(
-        bucket, args.prefix[0], tmpdir
-    )
+        for file in waveheight_tiff_filenames:
+            taskid = upload_to_gee(
+                file,
+                bucket,
+                args.assetfolder[0] + "/waveheight/" + file.replace(".tif", ""),
+                wait=False,
+                force=True,
+            )
+            logging.info(f"Added task {taskid}")
+            taskids.append(taskid)
 
-    for file in waveheight_tiff_filenames:
-        taskid = upload_to_gee(
-            file,
-            bucket,
-            args.assetfolder[0] + "/waveheight/" + file.replace(".tif", ""),
-            wait=False,
-            force=True,
-        )
-        logging.info(f"Added task {taskid}")
-        taskids.append(taskid)
+        # Wait for all the tasks to finish
+        wait_gee_tasks(taskids)
 
-    # Wait for all the tasks to finish
-    wait_gee_tasks(taskids)
+    if not args.skip_currents:
 
-    # This should result in flowmap tiff files
-    # The currents from glossis are converted to a tiff file that contains the flowmap  (rgb-encoded vector field)
-    flowmap_task_ids = []
-    flowmap_tiffs = []
-    for current_asset in current_assets:
-        flowmap_tiff = Path(current_asset).with_suffix('.tif').name
-        flowmap_tiffs.append(flowmap_tiff)
-        task_id = export_flowmap(current_asset, bucket)
-        flowmap_task_ids.append(task_id)
-    wait_gee_tasks(flowmap_task_ids)
+        # This should result in flowmap tiff files
+        # The currents from glossis are converted to a tiff file that contains the flowmap  (rgb-encoded vector field)
+        flowmap_task_ids = []
+        flowmap_tiffs = []
+        for current_asset in current_assets:
+            flowmap_tiff = Path(current_asset).with_suffix('.tif').name
+            flowmap_tiffs.append(flowmap_tiff)
+            task_id = export_flowmap(current_asset, bucket)
+            flowmap_task_ids.append(task_id)
+        wait_gee_tasks(flowmap_task_ids)
 
-    # This should result in flowmap tiles in a bucket
-    # The flowmaps are tiled using a rather specific tile format
-    # These  are  uploaded to the public bucket
-    flowmap_tiles_task_ids = []
-    for flowmap_tiff in flowmap_tiffs:
-        tile_dir = generate_wgs84_tiles(flowmap_tiff)
-        upload_dir_to_bucket(public_bucket, source_dir_name=tile_dir, destination_dir_name='flowmaps')
-        # TODO: how do we know which tiles are available in backend
+        for flowmap_tiff in flowmap_tiffs:
+            download_blob(bucket, 'flowmap/glossis' / flowmap_tiff, flowmap_tiff)
+
+        # This should result in flowmap tiles in a bucket
+        # The flowmaps are tiled using a rather specific tile format
+        # These  are  uploaded to the public bucket
+        flowmap_tiles_task_ids = []
+        for flowmap_tiff in flowmap_tiffs:
+            tile_dir = generate_wgs84_tiles(flowmap_tiff)
+            upload_dir_to_bucket(public_bucket, source_dir_name=tile_dir, destination_dir_name='flowmaps')
+            # TODO: how do we know which tiles are available in backend
