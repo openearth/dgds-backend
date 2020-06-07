1882e4b852e296633cf44b95d3613c23b92bef87
Maarten Pronk <git@evetion.nl>
2020-06-07 14:49:46 +0200

    Fixed comment.

--- datasets/glossis/glossis.py
+++ datasets/glossis/glossis.py
@@ -7,7 +7,14 @@ from os import makedirs
 from os.path import exists
 from shutil import rmtree
 
-from utils import fm_to_tiff, list_blobs, upload_to_gee, wait_gee_tasks, upload_dir_to_bucket, download_blob
+from utils import (
+    fm_to_tiff,
+    list_blobs,
+    upload_to_gee,
+    wait_gee_tasks,
+    upload_dir_to_bucket,
+    download_blob,
+)
 from waterlevel import create_water_level_astronomical_band
 from waveheight import glossis_waveheight_to_tiff
 from wind import glossis_wind_to_tiff
@@ -28,16 +35,24 @@ if __name__ == "__main__":
         "prefix", type=str, nargs=1, help="Input folder/prefix", default="fews_glossis/"
     )
     parser.add_argument("assetfolder", type=str, nargs=1, help="GEE asset")
-    parser.add_argument("--skip-waterlevel", dest='skip_waterlevel', default=False, action='store_true')
-    parser.add_argument("--skip-wind", dest='skip_wind', default=False, action='store_true')
-    parser.add_argument("--skip-currents", dest='skip_currents', default=False, action='store_true')
-    parser.add_argument("--skip-waves", dest='skip_waves', default=False, action='store_true')
+    parser.add_argument(
+        "--skip-waterlevel", dest="skip_waterlevel", default=False, action="store_true"
+    )
+    parser.add_argument(
+        "--skip-wind", dest="skip_wind", default=False, action="store_true"
+    )
+    parser.add_argument(
+        "--skip-currents", dest="skip_currents", default=False, action="store_true"
+    )
+    parser.add_argument(
+        "--skip-waves", dest="skip_waves", default=False, action="store_true"
+    )
 
     args = parser.parse_args()
     logging.info(args.bucket)
 
     bucket = args.bucket[0]
-    public_bucket = args.bucket[0] + '-public'
+    public_bucket = args.bucket[0] + "-public"
     prefix = args.prefix[0]
 
     # Setup directory
@@ -96,14 +111,10 @@ if __name__ == "__main__":
         # these assets are needed by the flowmap
         current_assets = []
         for file in current_tiff_filenames:
-            current_asset = args.assetfolder[0] + "/currents/" + file.replace(".tif", "")
-            taskid = upload_to_gee(
-                file,
-                bucket,
-                current_asset,
-                wait=False,
-                force=True,
+            current_asset = (
+                args.assetfolder[0] + "/currents/" + file.replace(".tif", "")
             )
+            taskid = upload_to_gee(file, bucket, current_asset, wait=False, force=True,)
             logging.info(f"Added task {taskid}")
             current_assets.append(current_asset)
             taskids.append(taskid)
@@ -149,20 +160,23 @@ if __name__ == "__main__":
         flowmap_task_ids = []
         flowmap_tiffs = []
         for current_asset in current_assets:
-            flowmap_tiff = pathlib.Path(current_asset).with_suffix('.tif').name
+            flowmap_tiff = pathlib.Path(current_asset).with_suffix(".tif").name
             flowmap_tiffs.append(flowmap_tiff)
             task_id = export_flowmap(current_asset, bucket)
             flowmap_task_ids.append(task_id)
         wait_gee_tasks(flowmap_task_ids)
 
-        for flowmap_tiff in flowmap_tiffs:
-            download_blob(bucket, str(pathlib.Path('flowmap/glossis') / flowmap_tiff), flowmap_tiff)
-
         # This should result in flowmap tiles in a bucket
         # The flowmaps are tiled using a rather specific tile format
         # These  are  uploaded to the public bucket
-        flowmap_tiles_task_ids = []
         for flowmap_tiff in flowmap_tiffs:
+            download_blob(
+                bucket,
+                str(pathlib.Path("flowmap/glossis") / flowmap_tiff),
+                flowmap_tiff,
+            )
             tile_dir = generate_wgs84_tiles(flowmap_tiff)
-            upload_dir_to_bucket(public_bucket, source_dir_name=tile_dir, destination_dir_name='flowmaps')
+            upload_dir_to_bucket(
+                public_bucket, source_dir_name=tile_dir, destination_dir_name="flowmaps"
+            )
             # TODO: how do we know which tiles are available in backend
